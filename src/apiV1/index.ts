1162e6e98d1e36b5cd315434f78fadacc445e132
Rasheed <a.rasheedalabi@gmail.com>
2021-11-07 07:22:22 +0100

    include health route

--- src/apiV1/index.ts
+++ src/apiV1/index.ts
@@ -4,6 +4,7 @@ import users from './users/user.route';
 
 const router: Router = Router();
 
+router.use('/health', (_, res) => res.status(200).json({live: true}));
 router.use('/', auth);
 router.use('/users', users);
 
